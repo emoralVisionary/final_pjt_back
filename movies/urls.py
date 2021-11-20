d720e1cd4d8f13497372ddcbbb3a870a38a56c0d
ruwan9 <ruwan9@kakao.com>
2021-11-20 17:21:11 +0900

    feat: auth added

--- movies/urls.py
+++ movies/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
     path('<int:movie_id>/', views.detail, name="detail"),
     
     # 영화에 대한 리뷰 목록
-    path('<int:movie_pk>/review/', views.review_list_create),
+    path('<int:movie_pk>/reviews/', views.review_list_create),
     # 영화에 대한 리뷰 수정/삭제
-    path('<int:movie_pk>/review/<int:review_pk>/', views.review_update_delete),
+    path('<int:movie_pk>/reviews/<int:review_pk>/update_delete/', views.review_update_delete),
 
     # 리뷰에 대한 댓글 목록
-    path('<int:review_pk>/review_comment/', views.review_comment_list_create),
+    path('<int:review_pk>/comments/', views.review_comment_list_create),
     # 리뷰에 대한 댓글 삭제
-    path('<int:review_pk>/review_comment/<int:review_comment_pk>/', views.review_comment_delete),
+    path('<int:review_pk>/delete/<int:review_comment_pk>/', views.review_comment_delete),
 ]
